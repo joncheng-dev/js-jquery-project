571ae7befed95493874831721c65d872e0fd7eec
Jonathan Cheng <jcheng461@gmail.com>
2022-03-04 00:27:49 +0900

    WIP: edit so columns work. Column on left shows survey questions, column on right shows picture. Add questions -- using radio & select boxes

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,13 @@
 $(document).ready(function () {
   // Upon submitting the form, do this --
   $("form#survey").submit(function (event) {
-    let result = decideCourse();
+    event.preventDefault();
+
+    const studyLvl = $("input:radio[name=studyLevel]:checked").val();
+    const learnType = $("input:radio[name=learningType]:checked").val();
+    console.log(studyLvl);
+    console.log(learnType);
+
+    // let result = decideCourse();
   });
 });
