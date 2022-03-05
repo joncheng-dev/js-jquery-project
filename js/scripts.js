8657087a6ced81673edfc7710bce8bd0b7f68a5a
Jonathan Cheng <jcheng461@gmail.com>
2022-03-06 00:58:50 +0900

    add user input form for age. Site branches content depending on user age.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,17 @@
 // Business Logic
-// Function to calculate age of user
-function ageCalculator(thisYear, birthYear) {
-  const userAge = thisYear - birthYear;
-  return userAge;
-}
-
-function todaysDate() {
-  var date = new Date();
-  var dd = String(date.getDate()).padStart(2, "0");
-  var mm = String(date.getMonth() + 1).padStart(2, "0"); //January is 0!
-  var yyyy = date.getFullYear();
-
-  date = yyyy + "-" + mm + "-" + dd;
-  return date;
-}
 
 // User Interface Logic
 $(document).ready(function () {
   // Upon submitting the form, do this --
   $("#dob").submit(function (event) {
-    const dob = $("#born").val();
-    console.log("User's Date of Birth: " + dob);
-
-    const today = todaysDate();
-    console.log("Today's Date: " + today);
-
-    const age = ageCalculator();
+    const age = parseInt($("#userAge").val());
+    if (age >= 17) {
+      $("#siteContent").show();
+      $("#gateKeeper").hide();
+    } else {
+      $("#under17").show();
+      $("#gateKeeper").hide();
+    }
 
     event.preventDefault();
   });
