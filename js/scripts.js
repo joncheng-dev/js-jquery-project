d31522223bb7a75b66ba2b97dabb359e0805b690
Jonathan Cheng <jcheng461@gmail.com>
2022-03-05 23:15:18 +0900

    WIP: erase progress. Add background image. Work on age check for entering website

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,32 @@
 // Business Logic
+// Function to calculate age of user
+function ageCalculator(thisYear, birthYear) {
+  const userAge = thisYear - birthYear;
+  return userAge;
+}
+
+function todaysDate() {
+  var date = new Date();
+  var dd = String(date.getDate()).padStart(2, "0");
+  var mm = String(date.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = date.getFullYear();
+
+  date = yyyy + "-" + mm + "-" + dd;
+  return date;
+}
 
 // User Interface Logic
 $(document).ready(function () {
   // Upon submitting the form, do this --
-  $("form#survey").submit(function (event) {
+  $("#dob").submit(function (event) {
+    const dob = $("#born").val();
+    console.log("User's Date of Birth: " + dob);
+
+    const today = todaysDate();
+    console.log("Today's Date: " + today);
+
+    const age = ageCalculator();
+
     event.preventDefault();
-
-    const studyLvl = $("input:radio[name=studyLevel]:checked").val();
-    const learnType = $("input:radio[name=learningType]:checked").val();
-    console.log(studyLvl);
-    console.log(learnType);
-
-    // let result = decideCourse();
   });
 });
